77b28d5511721830d08c029381820a903d5b19b9
swanand reddy <swanand.l@valnetinc.com>
2021-02-22 02:18:55 -0500

    using query params

--- public/index.php
+++ public/index.php
@@ -1,22 +1,66 @@
 <?php
+ ini_set('display_errors', 1);
+ ini_set('display_startup_errors', 1);
+ error_reporting(E_ALL);
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use App\Controller\ApiFetchController;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Middlewares\TrailingSlash;
 
 $app = AppFactory::create();
-$app->addErrorMiddleware(false, false, false);
+$app->add(new TrailingSlash(true));
+//$app->addRoutingMiddleware();
+	
+// Define Custom Error Handler
+$customErrorHandler = function (
+	Request $request,
+	Throwable $exception,
+	bool $displayErrorDetails,
+	bool $logErrors,
+	bool $logErrorDetails,
+	?LoggerInterface $logger = null
+) use ($app) {
+	$logger->error($exception->getMessage());
+	
+	$payload = ['error' => $exception->getMessage()];
+	
+	$response = $app->getResponseFactory()->createResponse();
+	$response->getBody()->write(
+		json_encode($payload, JSON_UNESCAPED_UNICODE)
+	);
+	
+	return $response;
+};
 
-$app->get('/',  ApiFetchController::class . ':listAllBooks');
+//$errorMiddleware  =
+$app->addErrorMiddleware(true, false, false);
+	
+//$errorMiddleware->setDefaultErrorHandler($customErrorHandler);
+	
+$middlewareHandleReq = function  (Request $request, RequestHandler $handler) {
+	$param = $request->getQueryParams();
+		
+	return  $handler->handle($request);
+	
+};
+$app->get('/list-all/',  ApiFetchController::class . ':listAllBooks')->add($middlewareHandleReq);
+	
+//$app->get('/per_page[/{number:.*}]',  ApiFetchController::class . ':listBookPerPage');  this is path param
 
-$app->get('/per_page[/{number:.*}]',  ApiFetchController::class . ':listBookPerPage');
+// this is GET param with query parameter
+$app->get('/list-per-page/[?per_page]',  ApiFetchController::class . ':listBookPerPage')->add($middlewareHandleReq);
 
-$app->get('/page[/{number:.*}]',  ApiFetchController::class . ':listBookByPageNumber');
+$app->get('/list-by-page/[?page]',  ApiFetchController::class . ':listBookByPageNumber');
 
-$app->get('/search[/{term:.*}]',  ApiFetchController::class . ':searchBooks');
+$app->get('/list-by-search/[?search]',  ApiFetchController::class . ':searchBooks');
 
-$app->get('/is_original[/{number:.*}]',  ApiFetchController::class . ':listBookByOriginal');
+$app->get('/list-by-original/[?is_original]',  ApiFetchController::class . ':listBookByOriginal');
 
-$app->get('/subject[/{identifier:.*}]',  ApiFetchController::class . ':listBookBySubject');
+$app->get('/list-by-subject/[?subject]',  ApiFetchController::class . ':listBookBySubject');
 
 $app->run();
